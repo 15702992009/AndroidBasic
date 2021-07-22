12cddfb156b5b01287daba185a86341895e4e8e4
wangjiabo <iswangjiabo@gmail.com>
2021-07-22 18:52:23 +0800

    first commit

---
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.java
@@ -0,0 +1,125 @@
+package com.example.criminalintent;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+/**
+ * 绑定
+ */
+public class CrimeListFragment extends Fragment {
+    private RecyclerView mCrimeRecyclerView;
+    private CrimeAdapter mAdapter;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // xml文件定位绑定View子类
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        // RecyclerView xml绑定到类对象
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        //设置 管理器，为什么是LinearLayout
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        updateUI();
+
+        return view;
+    }
+
+    /**
+     * 视图创建与数据绑定
+     */
+    private void updateUI() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+        mAdapter = new CrimeAdapter(crimes);
+        mCrimeRecyclerView.setAdapter(mAdapter);
+    }
+
+    /**
+     * bind list_item_view first
+     */
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private ImageView mSolvedImageView;
+        private Crime mCrime;
+
+        public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_crime, parent, false));
+//            itemView is located in  RecyclerView.ViewHolder class variable
+//            public abstract static class ViewHolder {
+//                @NonNull
+//                public final View itemView;
+            mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
+            mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+            mSolvedImageView = (ImageView) itemView.findViewById(R.id.crime_imageView);
+            itemView.setOnClickListener(this);
+
+        }
+
+        public void bind(Crime crime) {
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getmTitle());
+            mDateTextView.setText(mCrime.getmDate().toString());
+            if (crime.ismSolved()) {
+                mSolvedImageView.setImageResource(R.drawable.loves);
+            } else {
+                mSolvedImageView.setImageResource(R.drawable.fire);
+            }
+
+            Log.d("VISIBLE", "" + crime.ismSolved());
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getActivity(),
+                    mCrime.getmTitle() + " clicked!", Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    /**
+     * extends RecyclerView.Adapter<CrimeHolder>  CrimeHolder   need be implemented first
+     */
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
+        @NonNull
+        @org.jetbrains.annotations.NotNull
+        @Override
+        /**
+         * Adapter绑定ViewHolder
+         */
+        public CrimeHolder onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new CrimeHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull CrimeListFragment.CrimeHolder holder, int position) {
+            Crime crime = mCrimes.get(position);
+            holder.bind(crime);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
+    }
+
+}
